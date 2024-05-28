6cde9893db46c26c4362e20ce2c58cda4ca92309
kunj-30 <kunjvgoyal30@gmail.com>
2024-05-28 20:32:11 +0530

    2nd exercise completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if(num < 0) return "ERROR";
+    var ans = "";
+    for(let i=0; i<num; i++){
+        ans += str;
+    }
 
+    return ans;
 };
 
 // Do not edit below this line
