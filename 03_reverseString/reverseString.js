6c467fa176124718b9e1889acd0da380de332e5d
kunj-30 <kunjvgoyal30@gmail.com>
2024-05-28 20:39:54 +0530

    03 exercise completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    // let ans = "";
+    // for(let i=str.length-1; i>=0; i--){
+    //     ans += str[i];
+    // }
+    // return ans;
 
+    return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
