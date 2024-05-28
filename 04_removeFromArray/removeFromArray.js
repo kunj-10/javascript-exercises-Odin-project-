9dbae9f10a63c6664d91a4b783c79bd6a962b67c
kunj-30 <kunjvgoyal30@gmail.com>
2024-05-28 20:50:54 +0530

    04 exercise completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
 const removeFromArray = function() {
+    let arr = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        let itemToRemove = arguments[i];
+        let index;
+        while ((index = arr.indexOf(itemToRemove)) !== -1) {
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
