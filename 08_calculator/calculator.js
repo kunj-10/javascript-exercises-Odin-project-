2f4f7362114060dbb50117b62be19273eb39a52c
kunj-30 <kunjvgoyal30@gmail.com>
2024-05-31 20:09:24 +0530

    08 exercise completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let s=0;
+	for(let i=0; i<a.length; i++){
+    s += a[i];
+  }
+  return s;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let mul=1;
+  for(let i=0; i<a.length; i++){
+    mul *= a[i];
+  }
+  return mul;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fact = 1;
+  while(a){
+    fact *= a;
+    a--;
+  }
+  return fact;
 };
 
 // Do not edit below this line
