68baee410d2060ac87cd28cb88f20b68417997fb
kunj-30 <kunjvgoyal30@gmail.com>
2024-05-31 20:02:12 +0530

    07 exercise completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let cls = ((num-32)*5)/9;
+  return parseFloat(cls.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let frnt = ((num*9)/5)+32;
+  return parseFloat(frnt.toFixed(1));
 };
 
 // Do not edit below this line
