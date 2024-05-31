6603f464ce8237ea2c2910618533191209ad76c6
kunj-30 <kunjvgoyal30@gmail.com>
2024-05-31 21:12:59 +0530

    09 exercise completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().split('').filter(val => /[a-zA-Z0-9]/.test(val)).join('');
+    return str === str.split('').reverse().join('');
 };
 
 // Do not edit below this line
