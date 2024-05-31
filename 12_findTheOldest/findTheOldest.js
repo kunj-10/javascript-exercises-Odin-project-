d1699a5f889581293c511c1bb63a7ad9dab33369
kunj-30 <kunjvgoyal30@gmail.com>
2024-05-31 22:36:34 +0530

    12 exercise completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    var ans;
+    var max = -1;
+    const currentYear = new Date().getFullYear();
+    for(var i=0; i<people.length; i++){
+        if('yearOfDeath' in people[i]){
+            if(max < (people[i].yearOfDeath - people[i].yearOfBirth)){
+                max = (people[i].yearOfDeath - people[i].yearOfBirth);
+                ans = people[i];
+            }
+        }else{
+            if(max < (currentYear - people[i].yearOfBirth)){
+                max = (currentYear - people[i].yearOfBirth);
+                ans = people[i];
+            }
+        }
+    }
+    return ans;
 };
 
 // Do not edit below this line
