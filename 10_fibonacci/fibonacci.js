b16fabeddcbd4d98c01cf0289bb48e99a2a696f8
kunj-30 <kunjvgoyal30@gmail.com>
2024-05-31 21:28:58 +0530

    10 exercise completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(isNaN(Number(num))) return "OOPS";
+    if(Number(num) > 0){
+        num = Number(num) - 1;
+        let a = 1;
+        let b = 1;
+        while(num){
+            let temp = a+b;
+            a = b;
+            b = temp;
+            num--;
+        }
+        return a;
+    }else if(Number(num) === 0) return 0;
+    return "OOPS";
 };
 
 // Do not edit below this line
